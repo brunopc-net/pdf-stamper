3fe147fbde876844303b9a58b425ac959d487e6c
Bruno PC <work@brunopc.net>
2024-11-29 14:52:04 -0500

    Main script refactoring

--- script.py
+++ script.py
@@ -7,31 +7,39 @@ from pdf_editor import PdfEditor
 # Logging
 import logging
 from log_config import LogConfig
-log_config = LogConfig()
-log_config.setup_logging()
+LogConfig().setup_logging()
 
 # Performance metrics
 import os
 import time
 import psutil
 
-process = psutil.Process(os.getpid())
-start_time = time.time()
+def with_metrics(func):
+    def metrics():
+        process = psutil.Process(os.getpid())
+        start_time = time.time()
 
-qr_data = f"""BEGIN:VCARD
-VERSION:3.0
-FN:John Doe
-TEL:+1234567890
-EMAIL:johndoe@example.com
-ADR:123 Business St, Suite 400, City, Country
-END:VCARD"""
-qr_image = QrBuilder(qr_data).export()
-pdf_editor = PdfEditor()
-qr_size = 10
-qr_pdf = pdf_editor.build_img_pdf(qr_image, x=5, y=5, w=qr_size, h=qr_size)
-pdf_editor.add_watermark(qr_pdf, 'input/original.pdf')
+        func()
 
-elapsed_time = time.time() - start_time
-mem_usage = process.memory_info().rss
-logging.info(f"Executed in {elapsed_time:.3f}s")
-logging.info(f"Memory usage: {mem_usage/ (1024 * 1024):.2f}mb")
+        elapsed_time = time.time() - start_time
+        mem_usage = process.memory_info().rss
+        logging.info(f"Executed in {elapsed_time:.3f}s")
+        logging.info(f"Memory usage: {mem_usage/ (1024 * 1024):.2f}mb")
+    return metrics
+
+def main():
+    qr_data = f"""BEGIN:VCARD
+    VERSION:3.0
+    FN:John Doe
+    TEL:+1234567890
+    EMAIL:johndoe@example.com
+    ADR:123 Business St, Suite 400, City, Country
+    END:VCARD"""
+    qr_image = QrBuilder(qr_data).export()
+    pdf_editor = PdfEditor()
+    qr_size = 10
+    qr_pdf = pdf_editor.build_img_pdf(qr_image, x=5, y=5, w=qr_size, h=qr_size)
+    pdf_editor.add_watermark(qr_pdf, 'input/original.pdf')
+
+execute = with_metrics(main)
+execute()
